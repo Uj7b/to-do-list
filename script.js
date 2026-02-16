62a7310cbc27210bb41ebc728321c4911282dec5
Uj7b <107117746+Uj7b@users.noreply.github.com>
2026-02-16 11:47:58 +0000

    16/02/2026

---
+++ script.js
@@ -0,0 +1,10 @@
+let search = document.getElementById("search");
+
+search.addEventListener("input", (ev) => {
+  console.log(ev.target.value);
+});
+
+function addTodo() {}
+function renderList() {}
+
+function filterTodo() {}
