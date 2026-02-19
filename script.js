3c850364964a246e3ae3e66f50dc5689a36a2649
Uj7b <107117746+Uj7b@users.noreply.github.com>
2026-02-19 08:29:49 +0000

    first Ramadan

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let search = document.getElementById("search");
+let add = document.getElementById("add");
+let apply = document.getElementById("apply");
+let cancel = document.getElementById("cancel");
+let addModal = document.getElementById("addTaskWindow");
 
 search.addEventListener("input", (ev) => {
   console.log(ev.target.value);
 });
 
-function addTodo() {}
+function addTodo() {
+  
+}
 function renderList() {}
 
 function filterTodo() {}
+add.addEventListener("click", (ev) => {
+  addModal.style.visibility = "visible";
+});
+cancel.addEventListener("click", (ev) => {
+  addModal.style.visibility = "hidden";
+});
+addModal.addEventListener("click", (e) => {
+  if (e.target === addModal) {
+
+    addModal.style.visibility = "hidden";
+  }
+  e.stopPropagation()
+});
+
+apply.addEventListener("click", addTodo())
